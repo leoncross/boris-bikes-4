1f13ea487c84936b71c39452ec956d11aaa933e3
leoncross <leon@lscross.co.uk>
2018-11-01 14:24:36 +0000

    challenge 8 - created release_bike method and one liner test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,3 +1,15 @@
+require "docking_station"
+
 describe DockingStation do
+  # it "responds to release_bike method" do
+  #   expect(subject).to respond_to (:release_bike)
+  # end
+
+  it { is_expected.to respond_to (:release_bike)}
+
+
+
+
+
 
 end
