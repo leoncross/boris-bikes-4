b05021a250d4524d4064ad7b4bdce5c579604aa2
leoncross <leon@lscross.co.uk>
2018-11-01 17:22:01 +0000

    Passed 11; passed the bike through the dock, and have the dock identify the bike stored. Passed tests

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,7 @@
 require_relative "./bike.rb"
 
 class DockingStation
+  attr_reader :bike
 
   def release_bike
     Bike.new
@@ -8,9 +9,7 @@ class DockingStation
 
   def dock(bike)
     @bike = bike
-
   end
-
 
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,22 +5,29 @@ describe DockingStation do
   #   expect(subject).to respond_to (:release_bike)
   # end
 
-  it { is_expected.to respond_to (:release_bike)}
+    it { is_expected.to respond_to (:release_bike)}
 
-  it "Releases a new bike upon .release_bike" do
-    bike = subject.release_bike
-    expect(bike.working?).to eq true
-  end
+    it "Releases a new bike upon .release_bike" do
+      bike = subject.release_bike
+      expect(bike.working?).to eq true
+    end
 
-  it { is_expected.to respond_to(:dock).with(1).argument}
+    it { is_expected.to respond_to(:dock).with(1).argument}
 
-  it "accepts an bike in the dock" do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+    it "accepts an bike in the dock" do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
 
+      it {is_expected.to respond_to(:bike)}
 
+      it "checks that the release_bike 'bike' can be docked and recalls the bike" do
+      station = DockingStation.new
+      bike = station.release_bike
+      docked_bike = station.dock(bike)
+      expect(station.bike).to eq station.dock(bike)
 
-  end
+    end
 
 
 
